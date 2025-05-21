8a473a1657ccec5f1ba48d6e475f0c8db9d0fef1
arun-vijay-vk <vijayakuamrarun07@gmail.com>
2025-05-21 12:46:39 +0530

    gscadd

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,13 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   return (
     <html lang="en-US">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="ePP3JgpbaLW2x-0DgqEksztWLKS2YtypHcxaFKUncAI"
+        />
+      </head>
+
       <body className={poppins.className}>
         <section className="mx-auto xl:max-w-[1300px]">
           <Navbar />
